82eddaa59ea8adb4f25cc78ff49b4d16491c3e6a
Noname <osunlong@gmail.com>
2018-01-11 19:54:21 +0000

    program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SMLOGX.Core;
+using CustomerCare.GUI.Components;
+
+namespace CustomerCare
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new frmLogin());
+        }
+    }
+}
