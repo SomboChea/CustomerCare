9e638c4afbc5335d5284b5117381c16f06738c42
Sambo Chea <sombochea100@gmail.com>
2018-01-28 11:55:00 +0700

    Commiting to Me

--- CustomerCare/GUI/Components/TextBox.cs
+++ CustomerCare/GUI/Components/TextBox.cs
@@ -4,15 +4,18 @@ using System.Windows.Forms;
 
 namespace CustomerCare.GUI.Components
 {
-    
     public partial class TextBox : UserControl
     {
-        //[Description("Test text displayed in the textbox"), Category("Alpha")]
-        public string TestColor { get => txtValue.Text; set => this.txtValue.Text = value; }
+        [Description("Test text displayed in the textbox"), Category("Alpha")]
+        public Color MyBackColor { get => txtValue.BackColor; set => txtValue.BackColor = value; }
 
-        //[Description("Test text displayed in the textbox"), Category("Alpha")]
-        public System.Drawing.Region Reg { get =>this.Region; set => this.Region=value; }
+        [Description("Test text displayed in the textbox"), Category("Alpha")]
+        public Color MyBorderColor { get => this.BackColor; set => this.BackColor = value; }
 
+        [Description("Test text displayed in the textbox"), Category("Alpha")]
+        public bool MyMulti { get => txtValue.Multiline; set => txtValue.Multiline = value; }
+
+        [Description("Test text displayed in the textbox"), Category("Alpha")]
         public string Value { get => txtValue.Text; set => txtValue.Text = value; }
 
         public TextBox()
