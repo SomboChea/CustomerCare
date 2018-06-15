5834d18b480a9393239c7ca8c237e1f12f733895
Sambo Chea <sombochea100@gmail.com>
2018-06-15 19:27:44 +0700

    add pro

--- CustomerCare/GUI/frmAddress.Designer.cs
+++ CustomerCare/GUI/frmAddress.Designer.cs
@@ -47,7 +47,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(24, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 22);
+            this.label1.Size = new System.Drawing.Size(189, 30);
             this.label1.TabIndex = 0;
             this.label1.Text = "City / Province";
             // 
@@ -56,17 +56,17 @@
             this.txtpro.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
             this.txtpro.Location = new System.Drawing.Point(28, 72);
             this.txtpro.Name = "txtpro";
-            this.txtpro.Size = new System.Drawing.Size(151, 31);
+            this.txtpro.Size = new System.Drawing.Size(151, 37);
             this.txtpro.TabIndex = 1;
             this.txtpro.TextChanged += new System.EventHandler(this.txtpro_TextChanged);
             // 
             // listPro
             // 
             this.listPro.FormattingEnabled = true;
-            this.listPro.ItemHeight = 22;
+            this.listPro.ItemHeight = 30;
             this.listPro.Location = new System.Drawing.Point(28, 119);
             this.listPro.Name = "listPro";
-            this.listPro.Size = new System.Drawing.Size(147, 202);
+            this.listPro.Size = new System.Drawing.Size(147, 184);
             this.listPro.TabIndex = 2;
             this.listPro.SelectedIndexChanged += new System.EventHandler(this.listPro_SelectedIndexChanged);
             // 
@@ -75,7 +75,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(216, 36);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 22);
+            this.label2.Size = new System.Drawing.Size(92, 30);
             this.label2.TabIndex = 0;
             this.label2.Text = "District";
             // 
@@ -83,17 +83,17 @@
             // 
             this.txtdis.Location = new System.Drawing.Point(220, 72);
             this.txtdis.Name = "txtdis";
-            this.txtdis.Size = new System.Drawing.Size(151, 31);
+            this.txtdis.Size = new System.Drawing.Size(151, 37);
             this.txtdis.TabIndex = 1;
             this.txtdis.TextChanged += new System.EventHandler(this.txtdis_TextChanged);
             // 
             // listDis
             // 
             this.listDis.FormattingEnabled = true;
-            this.listDis.ItemHeight = 22;
+            this.listDis.ItemHeight = 30;
             this.listDis.Location = new System.Drawing.Point(220, 119);
             this.listDis.Name = "listDis";
-            this.listDis.Size = new System.Drawing.Size(147, 202);
+            this.listDis.Size = new System.Drawing.Size(147, 184);
             this.listDis.TabIndex = 2;
             this.listDis.SelectedIndexChanged += new System.EventHandler(this.listDis_SelectedIndexChanged);
             // 
@@ -102,7 +102,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(410, 36);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(107, 22);
+            this.label3.Size = new System.Drawing.Size(143, 30);
             this.label3.TabIndex = 0;
             this.label3.Text = "Commune";
             // 
@@ -110,17 +110,17 @@
             // 
             this.txtcomm.Location = new System.Drawing.Point(414, 72);
             this.txtcomm.Name = "txtcomm";
-            this.txtcomm.Size = new System.Drawing.Size(151, 31);
+            this.txtcomm.Size = new System.Drawing.Size(151, 37);
             this.txtcomm.TabIndex = 1;
             this.txtcomm.TextChanged += new System.EventHandler(this.txtcomm_TextChanged);
             // 
             // listComm
             // 
             this.listComm.FormattingEnabled = true;
-            this.listComm.ItemHeight = 22;
+            this.listComm.ItemHeight = 30;
             this.listComm.Location = new System.Drawing.Point(414, 119);
             this.listComm.Name = "listComm";
-            this.listComm.Size = new System.Drawing.Size(147, 202);
+            this.listComm.Size = new System.Drawing.Size(147, 184);
             this.listComm.TabIndex = 2;
             // 
             // btnPro
@@ -155,9 +155,9 @@
             // 
             // frmAddress
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(628, 396);
+            this.ClientSize = new System.Drawing.Size(607, 395);
             this.Controls.Add(this.btnComm);
             this.Controls.Add(this.btnDis);
             this.Controls.Add(this.btnPro);
